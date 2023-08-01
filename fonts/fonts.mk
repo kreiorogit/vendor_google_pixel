26e20d6fae2af7f1158888432c44d4ccb6d849e0
Isaiah <lqlp0011@gmail.com>
2023-08-01 10:49:34 +0800

    Update fonts.mk

--- fonts/fonts.mk
+++ fonts/fonts.mk
@@ -1,3 +1,3 @@
 # Fonts
 PRODUCT_COPY_FILES += \
-    vendor/google/pixel/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+    vendor/gapps/common/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
